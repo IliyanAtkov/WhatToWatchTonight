c2ec0494417c0f5dfa7e27d865c137f198c73871
Producenta <Producenta@Producentas-Mac.local>
2016-01-31 21:08:58 +0200

    Created default iOS project

---
+++ WhatToWatchTonight/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WhatToWatchTonight
+//
+//  Created by Producenta on 1/31/16.
+//  Copyright © 2016 Iliyan Atkov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WhatToWatchTonight/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WhatToWatchTonight
+//
+//  Created by Producenta on 1/31/16.
+//  Copyright © 2016 Iliyan Atkov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
